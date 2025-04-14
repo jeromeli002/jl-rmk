ed9c231343042d67ecd2c3a3e63a8a22a3c9ee79
U-BGS-01\Administrator <532192607@qq.com>
2025-04-14 15:36:04 +0800

    cut

---
+++ examples/use_rust/nrf52840_cut/src/keymap.rs
@@ -0,0 +1,38 @@
+use rmk::action::{EncoderAction, KeyAction};
+use rmk::{a, encoder, k, layer, mo};
+pub(crate) const COL: usize = 4;
+pub(crate) const ROW: usize = 4;
+pub(crate) const NUM_LAYER: usize = 2;
+pub(crate) const NUM_ENCODER: usize = 3;
+
+#[rustfmt::skip]
+pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
+    [
+        layer!([
+            [k!(Tab), k!(Q), k!(W), k!(E)],
+            [k!(Escape), k!(A), k!(S), k!(D)],
+            [k!(LShift), k!(Z), k!(X), k!(C)],
+            [k!(LCtrl), k!(LGui), k!(LAlt), a!(No)]
+        ]),
+        layer!([
+            [k!(Tab), k!(Q), k!(W), k!(E)],
+            [k!(Escape), k!(A), k!(S), k!(D)],
+            [k!(LShift), k!(Z), k!(X), k!(C)],
+            [k!(LCtrl), k!(LGui), k!(LAlt), a!(No)]
+        ]),
+    ]
+}
+
+pub const fn get_default_encoder_map() -> [[EncoderAction; NUM_ENCODER]; NUM_LAYER] {
+    [
+        [
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+        ],[
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+            encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+        ],
+    ]
+}
